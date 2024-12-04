2411c65befb750d18a901193e6d0d07dd5627cf7
Alim Mahmudov <alimmahmudov27@gmail.com>
2024-12-04 22:20:47 +0600

    header

--- src/appPages/site/layout/header/Header.tsx
+++ src/appPages/site/layout/header/Header.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import scss from "./Header.module.scss";
+import logo from "@/shared/assets/images/logo.svg";
+import Image from "next/image";
+import { LuPhoneCall } from "react-icons/lu";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header id={scss.Header}>
+      <div className="container">
+        <div className={scss.header}>
+          <div className={scss.block}>
+            <Image src={logo} alt="" />
+            <div className={scss.box}>
+              <div className={scss.teg}>
+                <a href="">Стрижки</a>
+                <a href="">награды</a>
+                <a href="">Отзывы</a>
+              </div>
+              <button>
+                <LuPhoneCall /> +996 708 000 000
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
